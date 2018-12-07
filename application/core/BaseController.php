9b839f3ebe07813a4a12b7047db18dba03b60f28
Zhangyayaa <1029390260@qq.com>
2018-12-07 16:08:00 +0800

    1607

--- application/core/BaseController.php
+++ application/core/BaseController.php
@@ -8,7 +8,7 @@ class BaseController extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		// 参数依次为 AppId, AppKey, MasterKey
-		Client::initialize("SgHcsYqoLaFTG0XDMD3Gtm0I-gzGzoHsz", "xdv2nwjUK5waNglFoFXkQcxP" ,"v3P5xzDa0b5CStO0xX0biHpT");
+		Client::initialize("YFzggloQWOnyQPwmXGnRHnGW-gzGzoHsz", "5pJ2hDHl7FOTWElqoEADa6kR" ,"bfQPDGLeIM8jFBakOJpPgoTA");
 		Client::useMasterKey(true);
 		Client::setStorage(new CookieStorage());
 	}
